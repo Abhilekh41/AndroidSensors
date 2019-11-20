b1d5d5b227b07d932b9723231831fb1da00e87ed
Bajasahay <adven.abhisahay@gmail.com>
2019-11-20 23:50:19 +0530

    resolving issues

--- app/src/main/java/com/abhilekh/myapplication/MainActivity.java
+++ app/src/main/java/com/abhilekh/myapplication/MainActivity.java
@@ -34,7 +34,7 @@ import com.abhilekh.myapplication.Helper.DatabaseHelper;
 import com.google.android.material.navigation.NavigationView;
 
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener,NavigationView.OnNavigationItemSelectedListener
+public class MainActivity extends AppCompatActivity implements SensorEventListener
 {
 
     private SensorManager sensorManager;
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView xGyrometerValue,yGyrometerValue,zGyrometerValue;
 
     //Magnetometer
-    TextView xMagnetometerValue,yMagnetometerValue,zMagnetometerValye;
+    TextView xMagnetometerValue,yMagnetometerValue,zMagnetometerValue;
 
     //Barometer
     TextView barometerValue;
@@ -88,32 +88,34 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        assigningValues();
         setContentView(R.layout.activity_main);
 
         databaseHelper = new DatabaseHelper(this);
-        drawerLayout = findViewById(R.id.drawerLayout);
 
-        navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        xAccelerometerValue = findViewById(R.id.xAccelerometerValue);
+        yAccelerometerValue = findViewById(R.id.yAccelerometerValue);
+        zAccelerometerValue = findViewById(R.id.zAccelerometerValue);
 
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.Open,R.string.Close);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+        xGyrometerValue = findViewById(R.id.xGyrometerValue);
+        yGyrometerValue = findViewById(R.id.yGyrometerValue);
+        zGyrometerValue = findViewById(R.id.zGyrometerValue);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        xMagnetometerValue = findViewById(R.id.xMagnometerValue);
+        yMagnetometerValue = findViewById(R.id.yMagnometerValue);
+        zMagnetometerValue = findViewById(R.id.zMagnometerValue);
 
-        if(savedInstanceState == null)
-        {
-            getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new AccelerometerActivity()).commit();
-            navigationView.setCheckedItem(R.id.AccelerometerTab);
-        }
+        photometerValue =  findViewById(R.id.PhotometerValue);
 
-        Log.d(TAG, "onCreate: Initializing Sensor Services");
+        barometerValue = findViewById(R.id.BarometerValue);
+
+        thermometerValue = findViewById(R.id.ThermometerValue);
+
+        hygrometerValue = findViewById(R.id.HygrometerValue);
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        setContentView(R.layout.accelerometer_layout);
+
         if(accelerometerSensor!=null)
         {
             sensorManager.registerListener(MainActivity.this,accelerometerSensor,
@@ -127,7 +129,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             yAccelerometerValue.setText("Accelerometer is not supported");
             zAccelerometerValue.setText("Accelerometer is not supported");
         }
-
 
         gyrometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         if(gyrometerSensor!=null)
@@ -156,7 +157,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Log.d(TAG, "onCreate: Magetometer is not supported on your device");
             xMagnetometerValue.setText("Magnetometer is not supported on your device");
             yMagnetometerValue.setText("Magnetometer is not supported on your device");
-            zMagnetometerValye.setText("Magnetometer is not supported on your device");
+            zMagnetometerValue.setText("Magnetometer is not supported on your device");
         }
 
         barometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
@@ -210,198 +211,126 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Log.d(TAG, "onCreate: Hygrometer is not supported on your device");
             hygrometerValue.setText("Hygrometer is not supported on your device");
         }
-        setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: Sensor Registration Done");
-     }
+
+
+    }
 
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent)
     {
-       Sensor sensor = sensorEvent.sensor;
-       Integer transactionid = 0;
-       if(sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-       {
-           transactionid = transactionid +1;
-           Accelerometer accelerometer = new Accelerometer(transactionid,sensorEvent.values[0],
-                   sensorEvent.values[1],sensorEvent.values[2]);
-           Log.d(TAG, "onSensorChanged:  X : "+accelerometer.getxValue()+
-                                             " Y : "+accelerometer.getyValue()+
-                                             " Z : "+accelerometer.getzValue());
-           xAccelerometerValue.setText("Accelerometer's X Co-ordinate : "+accelerometer.getxValue());
-           yAccelerometerValue.setText("Accelerometer's Y Co-ordinate : "+accelerometer.getyValue());
-           zAccelerometerValue.setText("Accelerometer's Z Co-ordinate : "+accelerometer.getzValue());
+        Sensor sensor = sensorEvent.sensor;
+        Integer transactionid = 0;
 
-           boolean result = databaseHelper.insertAccelerometerData(accelerometer);
+        if(sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        {
+            transactionid = transactionid +1;
+            Accelerometer accelerometer = new Accelerometer(transactionid,sensorEvent.values[0],
+                    sensorEvent.values[1],sensorEvent.values[2]);
+            Log.d(TAG, "onSensorChanged:  X : "+accelerometer.getxValue()+
+                    " Y : "+accelerometer.getyValue()+
+                    " Z : "+accelerometer.getzValue());
+            xAccelerometerValue.setText("Accelerometer's X Co-ordinate : "+accelerometer.getxValue());
+            yAccelerometerValue.setText("Accelerometer's Y Co-ordinate : "+accelerometer.getyValue());
+            zAccelerometerValue.setText("Accelerometer's Z Co-ordinate : "+accelerometer.getzValue());
 
-           Log.d(TAG, "Accelerometer DB Insertion :" +result);
+            boolean result = databaseHelper.insertAccelerometerData(accelerometer);
 
-       }
-       else if(sensor.getType() == Sensor.TYPE_GYROSCOPE)
-       {
-           transactionid = transactionid+1;
-           Gyrometer gyrometer = new Gyrometer(transactionid,sensorEvent.values[0],
-                   sensorEvent.values[1],sensorEvent.values[2]);
-           Log.d(TAG, "onSensorChanged:  X : "+gyrometer.getxValue()+
-                   " Y : "+gyrometer.getyValue()+
-                   " Z : "+gyrometer.getzValue());
-           xGyrometerValue.setText("GyroMeter's X Co-ordinate : "+gyrometer.getxValue());
-           yGyrometerValue.setText("GyroMeter's Y Co-ordinate : "+gyrometer.getyValue());
-           zGyrometerValue.setText("GyroMeter's Z Co-ordinate : "+gyrometer.getzValue());
+            Log.d(TAG, "Accelerometer DB Insertion :" +result);
 
+        }
 
-           boolean result =  databaseHelper.insertGyrometerData(gyrometer);
-
-           Log.d(TAG, "Gyrometer DB Insertion :" +result);
-       }
-
-       else if(sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
-       {
-           setContentView(R.layout.magnometer_layout);
-           transactionid = transactionid+1;
-           Magnometer magnometer = new Magnometer(transactionid,sensorEvent.values[0],
-                   sensorEvent.values[1],sensorEvent.values[2]);
-           Log.d(TAG, "onSensorChanged:  X : "+magnometer.getxValue()+
-                   " Y : "+magnometer.getyValue()+
-                   " Z : "+magnometer.getzValue());
-           xMagnetometerValue.setText("Magnometer's X Co-ordinate : "+magnometer.getxValue());
-           yMagnetometerValue.setText("Magnometer's Y Co-ordinate : "+magnometer.getyValue());
-           zMagnetometerValye.setText("Magnometer's Z Co-ordinate : "+magnometer.getzValue());
+        else if(sensor.getType() == Sensor.TYPE_GYROSCOPE)
+        {
+            transactionid = transactionid+1;
+            Gyrometer gyrometer = new Gyrometer(transactionid,sensorEvent.values[0],
+                    sensorEvent.values[1],sensorEvent.values[2]);
+            Log.d(TAG, "onSensorChanged:  X : "+gyrometer.getxValue()+
+                    " Y : "+gyrometer.getyValue()+
+                    " Z : "+gyrometer.getzValue());
+            xGyrometerValue.setText("GyroMeter's X Co-ordinate : "+gyrometer.getxValue());
+            yGyrometerValue.setText("GyroMeter's Y Co-ordinate : "+gyrometer.getyValue());
+            zGyrometerValue.setText("GyroMeter's Z Co-ordinate : "+gyrometer.getzValue());
 
 
-           boolean result =  databaseHelper.insertMagnometerData(magnometer);
+            boolean result =  databaseHelper.insertGyrometerData(gyrometer);
 
-           Log.d(TAG, "Magnometer DB Insertion :" +result);
-       }
+            Log.d(TAG, "Gyrometer DB Insertion :" +result);
+        }
 
-       else if(sensor.getType() == Sensor.TYPE_RELATIVE_HUMIDITY)
-       {
-           transactionid = +1;
-           Hygrometer hygrometer = new Hygrometer(transactionid,sensorEvent.values[0]);
-           Log.d(TAG, "onSensorChanged:  Hygrometer : "+sensorEvent.values[0]);
-           hygrometerValue.setText("Humidity : "+sensorEvent.values[0]);
+        else if(sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+        {
+            transactionid = transactionid+1;
+            Magnometer magnometer = new Magnometer(transactionid,sensorEvent.values[0],
+                    sensorEvent.values[1],sensorEvent.values[2]);
+            Log.d(TAG, "onSensorChanged:  X : "+magnometer.getxValue()+
+                    " Y : "+magnometer.getyValue()+
+                    " Z : "+magnometer.getzValue());
+            xMagnetometerValue.setText("Magnometer's X Co-ordinate : "+magnometer.getxValue());
+            yMagnetometerValue.setText("Magnometer's Y Co-ordinate : "+magnometer.getyValue());
+            zMagnetometerValue.setText("Magnometer's Z Co-ordinate : "+magnometer.getzValue());
 
-           boolean result = databaseHelper.insertHygrometerData(hygrometer);
 
-           Log.d(TAG, "Hygrometer DB Insertion :" +result);
-       }
+            boolean result =  databaseHelper.insertMagnometerData(magnometer);
 
-       else if(sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE)
-       {
-           transactionid = transactionid+1;
-           Thermometer thermometer = new Thermometer(transactionid,sensorEvent.values[0]);
-           Log.d(TAG, "onSensorChanged:  Thermometer : "+sensorEvent.values[0]);
+            Log.d(TAG, "Magnometer DB Insertion :" +result);
+        }
 
-           thermometerValue.setText("Temperature : "+thermometer.getReading());
+        else if(sensor.getType() == Sensor.TYPE_LIGHT)
+        {
+            transactionid =+1;
+            Photometer photometer = new Photometer(transactionid,sensorEvent.values[0]);
+            Log.d(TAG, "onSensorChanged:  Photometer : "+sensorEvent.values[0]);
 
-           boolean result = databaseHelper.insertThermometerData(thermometer);
+            photometerValue.setText("Light Intensity : "+photometer.getReading());
 
-           Log.d(TAG, "Thermometer DB insertion :" + result);
-       }
+            boolean result = databaseHelper.insertPhotometerData(photometer);
 
-       else if(sensor.getType() == Sensor.TYPE_LIGHT)
-       {
-           transactionid =+1;
-           Photometer photometer = new Photometer(transactionid,sensorEvent.values[0]);
-           Log.d(TAG, "onSensorChanged:  Photometer : "+sensorEvent.values[0]);
+            Log.d(TAG, "Photometer DB Insertion :" +result);
+        }
 
-           photometerValue.setText("Light Intensity : "+photometer.getReading());
+        else if(sensor.getType() == Sensor.TYPE_RELATIVE_HUMIDITY)
+        {
+            transactionid = +1;
+            Hygrometer hygrometer = new Hygrometer(transactionid,sensorEvent.values[0]);
+            Log.d(TAG, "onSensorChanged:  Hygrometer : "+sensorEvent.values[0]);
+            hygrometerValue.setText("Humidity : "+sensorEvent.values[0]);
 
-           boolean result = databaseHelper.insertPhotometerData(photometer);
+            boolean result = databaseHelper.insertHygrometerData(hygrometer);
 
-           Log.d(TAG, "Photometer DB Insertion :" +result);
-       }
+            Log.d(TAG, "Hygrometer DB Insertion :" +result);
+        }
 
-       else if(sensor.getType() == Sensor.TYPE_PRESSURE)
-       {
-           transactionid = +1;
-           Barometer barometer = new Barometer(transactionid,sensorEvent.values[0]);
-           Log.d(TAG, "onSensorChanged:  Hygrometer : "+sensorEvent.values[0]);
-           hygrometerValue.setText("Humidity : "+sensorEvent.values[0]);
+        else if(sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE)
+        {
+            transactionid = transactionid+1;
+            Thermometer thermometer = new Thermometer(transactionid,sensorEvent.values[0]);
+            Log.d(TAG, "onSensorChanged:  Thermometer : "+sensorEvent.values[0]);
 
-           boolean result = databaseHelper.insertBarometerData(barometer);
+            thermometerValue.setText("Temperature : "+thermometer.getReading());
 
-           Log.d(TAG, "Barometer DB Insertion :" +result);
-       }
-        setContentView(R.layout.activity_main);
+            boolean result = databaseHelper.insertThermometerData(thermometer);
+
+            Log.d(TAG, "Thermometer DB insertion :" + result);
+        }
+
+        else if(sensor.getType() == Sensor.TYPE_PRESSURE)
+        {
+            transactionid = +1;
+            Barometer barometer = new Barometer(transactionid,sensorEvent.values[0]);
+            Log.d(TAG, "onSensorChanged:  Hygrometer : "+sensorEvent.values[0]);
+            hygrometerValue.setText("Humidity : "+sensorEvent.values[0]);
+
+            boolean result = databaseHelper.insertBarometerData(barometer);
+
+            Log.d(TAG, "Barometer DB Insertion :" +result);
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i)
     {
 
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(actionBarDrawerToggle.onOptionsItemSelected(item))
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
-    {
-        switch (menuItem.getItemId())
-        {
-            case R.id.AccelerometerTab:
-                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new AccelerometerActivity()).commit();
-                break;
-            case R.id.GyrometerTab:
-                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new GyrometerActivity()).commit();
-                break;
-            case R.id.HygrometerTab:
-                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new HygrometerActivity()).commit();
-                break;
-            case R.id.PhotometerTab:
-                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new PhotometerActivity()).commit();
-                break;
-            case R.id.BarometerTab:
-                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new BarometerActivity()).commit();
-                break;
-            case R.id.MagnometerTab:
-                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new MagnometerActivity()).commit();
-                break;
-            case R.id.ThermometerTab:
-                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new ThermometerActivity()).commit();
-                break;
-        }
-
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    private void assigningValues()
-    {
-        setContentView(R.layout.accelerometer_layout);
-        xAccelerometerValue = findViewById(R.id.xAccelerometerValue);
-        yAccelerometerValue = findViewById(R.id.yAccelerometerValue);
-        zAccelerometerValue = findViewById(R.id.zAccelerometerValue);
-
-        setContentView(R.layout.gyrometer_layout);
-        xGyrometerValue = findViewById(R.id.xGyrometerValue);
-        yGyrometerValue = findViewById(R.id.yGyrometerValue);
-        zGyrometerValue = findViewById(R.id.zGyrometerValue);
-
-
-        setContentView(R.layout.magnometer_layout);
-        xMagnetometerValue = findViewById(R.id.xMagnometerValue);
-        yMagnetometerValue = findViewById(R.id.yMagnometerValue);
-        zMagnetometerValye = findViewById(R.id.zMagnometerValue);
-
-
-        setContentView(R.layout.photometer_layout);
-        photometerValue =  findViewById(R.id.photometerValue);
-
-        setContentView(R.layout.barometer_layout);
-        barometerValue = findViewById(R.id.barometerValue);
-
-        setContentView(R.layout.thermometer_layout);
-        thermometerValue = findViewById(R.id.thermometerValue);
-
-        setContentView(R.layout.hygrometer_layout);
-        hygrometerValue = findViewById(R.id.hygrometerValue);
     }
 }
