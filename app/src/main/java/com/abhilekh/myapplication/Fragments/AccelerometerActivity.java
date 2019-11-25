850d39c1dd6e5ceae21300ca488d6178556c66ca
Bajasahay <adven.abhisahay@gmail.com>
2019-11-25 23:07:21 +0530

    updated

--- app/src/main/java/com/abhilekh/myapplication/Fragments/AccelerometerActivity.java
+++ app/src/main/java/com/abhilekh/myapplication/Fragments/AccelerometerActivity.java
@@ -13,7 +13,6 @@ import com.abhilekh.myapplication.R;
 
 public class AccelerometerActivity extends Fragment
 {
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
